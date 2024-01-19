a611ccb5930dc6ccf397a2461a82b58030a29ad6
LucasBil <Lucas.Billy@etu.univ-savoie.fr>
2024-01-19 10:32:15 +0100

    Initial Commit

---
+++ SwiftUI_101/SwiftUI_101App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_101App.swift
+//  SwiftUI_101
+//
+//  Created by Lucas BILLY on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_101App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
