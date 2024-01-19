5a3cb2549b0db715a25dd976a50ab6e3d49830f2
LucasBil <Lucas.Billy@etu.univ-savoie.fr>
2024-01-19 10:32:15 +0100

    Initial Commit

---
+++ SwiftUI_101/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_101
+//
+//  Created by Lucas BILLY on 1/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
