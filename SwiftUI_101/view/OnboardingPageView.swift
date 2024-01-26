869ad8471c82021ac850c215b5715d0e831564b5
LucasBil <LucasBilly@etu.univ-savoie.fr>
2024-01-26 12:02:57 +0100

    tp2

---
+++ SwiftUI_101/view/OnboardingPageView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingPageView.swift
+//  SwiftUI_101
+//
+//  Created by Lucas BILLY on 1/26/24.
+//
+
+import SwiftUI
+
+struct OnboardingPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingPageView()
+}
---
+++ SwiftUI_101/view/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  SwiftUI_101
+//
+//  Created by Lucas BILLY on 1/26/24.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
