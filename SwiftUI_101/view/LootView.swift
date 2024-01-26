18231f4471c9200efce20936a4f01ecb362c542f
LucasBil <LucasBilly@etu.univ-savoie.fr>
2024-01-26 15:41:09 +0100

    suite

---
+++ SwiftUI_101/view/LootView.swift
@@ -0,0 +1,18 @@
+//
+//  LootView.swift
+//  SwiftUI_101
+//
+//  Created by Lucas BILLY on 1/26/24.
+//
+
+import SwiftUI
+
+struct LootView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LootView()
+}
---
+++ SwiftUI_101/view/WishListView.swift
@@ -0,0 +1,18 @@
+//
+//  WishListView.swift
+//  SwiftUI_101
+//
+//  Created by Lucas BILLY on 1/26/24.
+//
+
+import SwiftUI
+
+struct WishListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WishListView()
+}
